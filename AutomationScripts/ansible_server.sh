8a97085b6aec87a277f07f995ca7ec84650989fb
Vandananm08 <vandanamachaknur@gmail.com>
2024-02-21 23:03:10 +0530

    dynamic script

--- AutomationScripts/ansible_server.sh
+++ AutomationScripts/ansible_server.sh
@@ -7,7 +7,7 @@ host2_pvt_ip=$2
 printf "Loaded Host 2 IP : ${host2_pvt_ip}"
 userName=$(whoami)
 printf "Current User : ${userName}"
-logFilePath=/home/${userName}/ansibleServer_$(date +%y%m%d%H%M%S).log
+logFilePath=/home/${userName}/ansibleServer_$(date +%y%m%d).log
 printf "Running Ansible-Server Installation Script....."
 printf "Use 'tail -f ${logFilePath}' in another terminal/session to see the log-tail of this script."
 
@@ -15,7 +15,6 @@ sudo su
 echo "Logging in as super user...." >> ${logFilePath}
 echo "Installing wget...." >> ${logFilePath}
 yum install wget -y
-clear
 echo "Downloading epel-7....." >> ${logFilePath}
 wget http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 echo "Downloaded epel-7....." >> ${logFilePath}
@@ -27,7 +26,7 @@ yum update -y
 echo "System Update Complete." >> ${logFilePath}
 printf "Installing Software Packages:\n Git \n Python\n Python-Devel\n Python-PIP\n Open-SSL\n Ansible\n" >> ${logFilePath}
 yum install git python python-devel python-pip openssl ansible -y
-printf "All Pacakges Installed." >> ${logFilePath}
+printf "All Packages Installed." >> ${logFilePath}
 
 echo "Ansible: Enabling Ansible Inventory..." >> ${logFilePath}
 sed -i 's@#inventory      = /etc/ansible/hosts@inventory      = /etc/ansible/hosts@g' /etc/ansible/ansible.cfg
@@ -35,14 +34,14 @@ echo "Ansible: Enabling root/sudo user..." >> ${logFilePath}
 sed -i 's@#sudo_user      = root@sudo_user      = root@g' /etc/ansible/ansible.cfg
 echo "Ansible: Adding New Host [devops1] : ${host1_pvt_ip}" >> ${logFilePath}
 echo "[devops1]" >> /etc/ansible/hosts
-echo "172.31.39.114" >> /etc/ansible/hosts
+echo "${host1_pvt_ip}" >> /etc/ansible/hosts
 echo "Ansible: Adding New Host [devops2] : ${host2_pvt_ip}" >> ${logFilePath}
 echo "[devops2]" >> /etc/ansible/hosts
-echo "172.31.47.182" >> /etc/ansible/hosts
+echo "${host2_pvt_ip}" >> /etc/ansible/hosts
 printf "Ansible: Adding New Host-Group [devops] : \n${host1_pvt_ip}\n${host2_pvt_ip}" >> ${logFilePath}
 echo "[devops]" >> /etc/ansible/hosts
-echo "172.31.39.114" >> /etc/ansible/hosts
-echo "172.31.47.182" >> /etc/ansible/hosts
+echo "${host1_pvt_ip}" >> /etc/ansible/hosts
+echo "${host2_pvt_ip}" >> /etc/ansible/hosts
 echo "Ansible: Adding Local-Host [local] to Inventory..." >> ${logFilePath}
 echo "[local]" >> /etc/ansible/hosts
 echo "localhost" >> /etc/ansible/hosts
@@ -64,11 +63,12 @@ yum update -y
 ssh-keygen -f /home/ansible/.ssh/id_rsa -N ''
 echo "Loading Key-Pairs to Target Servers....." >> ${logFilePath}
 cd .ssh
-echo "yes" | ssh-copy-id ansible@172.31.39.114
-sshpass -p "anisble1234" ssh-copy-id ansible@172.31.39.114
-echo "yes" | ssh-copy-id ansible@172.31.47.182
-sshpass -p "anisble1234" ssh-copy-id ansible@172.31.47.182
+echo "yes" | ssh-copy-id ansible@${host1_pvt_ip}
+sshpass -p "anisble1234" ssh-copy-id ansible@${host1_pvt_ip}
+echo "yes" | ssh-copy-id ansible@${host2_pvt_ip}
+sshpass -p "anisble1234" ssh-copy-id ansible@${host2_pvt_ip}
 echo "Loading Key-Pairs to Target Servers : COMPLETED" >> ${logFilePath}
 echo "Ansible-Server : Setup Completed" >> ${logFilePath}
+echo "Ansible-Server : Setup Completed"
 
 
